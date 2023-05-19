b38f58a55ae82d60340407a55c088fdf568a77f5
danieljuma5 <jumadaniel275@gmail.com>
2023-05-20 00:16:57 +0300

    further practice

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+   return false if self.nil? || self.empty?
+    self.end_with?('.')
   end
 
   def question?
-
+    return false if self.nil? || self.empty?
+      self.end_with?("?")
   end
 
   def exclamation?
+    return false unless self.is_a?(String)
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    self.split(/[.!?]+/).count
   end
 end
+
+str1 = String.new("This is a string! It has three sentences. Right?")
